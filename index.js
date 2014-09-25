9312f99797b6cc97ca8b40eb22c4f0e4863e62b8
Luke Bond <luke.n.bond@gmail.com>
2014-09-25 14:45:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var EventEmitter = require('events').EventEmitter;
+var CustomError = require('error-plus');
+var Redis = require('redis');
+var util = require('util');
+
+function RedisQueueSizePoller(options, queueNames) {
+  if (!(this instanceof RedisQueueSizePoller)) {
+    return new RedisQueueSizePoller(options, queueNames);
+  }
+
+  this.pollInterval = options.pollInterval || 5000;
+  this.redisClient = Redis.createClient(
+    { port: options.port, host: options.host,
+      password: options.password });
+  this.queueNames = queueNames;
+  this.queueSizes = {};
+  this.stopping = false;
+  this.timerHandle = null;
+  process.nextTick(this.pollQueueSizes.bind(this));
+}
+
+util.inherits(RedisQueueSizePoller, EventEmitter);
+
+RedisQueueSizePoller.prototype.getQueueSizes = function () {
+  return this.queueSizes;
+};
+
+RedisQueueSizePoller.prototype.pollQueueSizes = function () {
+  var multi = this.redisClient.multi();
+  this.queueNames.forEach(function (queueName) {
+    multi.llen(queueName);
+  });
+  var self = this;
+  multi.exec(function (err, sizes) {
+    if (err) {
+      self.emit(new CustomError(err, 'Error retrieving Redis queue sizes'));
+      return;
+    }
+    self.queueNames.forEach(function (el, i) {
+      self.queueSizes[el] = sizes[i];
+    });
+  });
+  if (!this.stopping) {
+    this.timerHandle = setTimeout(this.pollQueueSizes.bind(this), this.pollInterval);
+  }
+};
+
+RedisQueueSizePoller.prototype.close = function (cb) {
+  if (this.timerHandle) {
+    clearTimeout(this.timerHandle);
+  }
+  this.stopping = true;
+  this.redisClient.quit(cb);
+};
+
+module.exports = RedisQueueSizePoller;
---
+++ test/test.js
@@ -0,0 +1,50 @@
+var RedisQueueSizePoller = require('../index');
+var Redis = require('redis');
+var test = require('tape');
+
+var redis = Redis.createClient();
+redis.flushall();
+
+test('Poll time defaults to 5s', function (t) {
+  var poller = new RedisQueueSizePoller({}, ['one', 'two', 'three']);
+  t.equal(poller.pollInterval, 5000, 'Poll time should default to 5 seconds');
+  poller.close();
+  t.end();
+});
+
+test('Results contain the queue names', function (t) {
+  var poller = new RedisQueueSizePoller({}, ['one', 'two', 'three']);
+  setTimeout(function () {
+    var sizes = poller.getQueueSizes();
+    t.equal(sizes.one, 0, 'Contains all the queue names');
+    t.equal(sizes.two, 0, 'Contains all the queue names');
+    t.equal(sizes.three, 0, 'Contains all the queue names');
+    poller.close();
+    t.end();
+  }, 500);
+});
+
+test('Returns correct queue sizes', function (t) {
+  var poller = new RedisQueueSizePoller({}, ['wow1', 'wow2', 'wow3']);
+  redis.multi()
+    .rpush('wow1', '1')
+    .rpush('wow1', '2')
+    .rpush('wow1', '3')
+    .rpush('wow1', '4')
+    .rpush('wow2', '1')
+    .rpush('wow2', '2')
+    .rpush('wow3', '1')
+    .rpush('wow3', '2')
+    .rpush('wow3', '3')
+    .exec(function (err, result) {
+      setTimeout(function () {
+        var sizes = poller.getQueueSizes();
+        t.equal(sizes.wow1, 4, 'Queue sizes are correct');
+        t.equal(sizes.wow2, 2, 'Queue sizes are correct');
+        t.equal(sizes.wow3, 3, 'Queue sizes are correct');
+        poller.close();
+        redis.quit();
+        t.end();
+      }, 5000);
+    });
+});
